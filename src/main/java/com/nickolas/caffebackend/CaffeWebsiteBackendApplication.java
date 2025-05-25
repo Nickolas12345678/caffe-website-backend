3aa5e1b0c5296729a81559d5d203365c4df917bb
Nickolas12345678 <mikolarisnak@gmail.com>
2025-05-25 14:32:07 +0300

    Added cart and order logic

--- src/main/java/com/nickolas/caffebackend/CaffeWebsiteBackendApplication.java
+++ src/main/java/com/nickolas/caffebackend/CaffeWebsiteBackendApplication.java
@@ -2,8 +2,10 @@ package com.nickolas.caffebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.nickolas.caffebackend.model")
 public class CaffeWebsiteBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(CaffeWebsiteBackendApplication.class, args);
